f494b4e9ce55be82a81ab61b3bf6957c86a2a0d2
Ian Forrest <ianforrest11@users.noreply.github.com>
2019-09-03 19:57:16 -0400

    added class for number variable

--- lambdata_ianforrest11/__init__.py
+++ lambdata_ianforrest11/__init__.py
@@ -14,5 +14,20 @@ def train_test_split(df, col):
 def NaN(df):
     return df.isna().sum()
 
+class Number:
+"""
+general representation of a number being even or odd
+"""
+
+    def __init__(self, number):
+    """
+    define number
+    """
+      self.number = number
+    
+    def odd_or_even(self)
+	return 'odd' if number%2 !==0 else 'even'
+
+
 ONES = pd.DataFrame(np.ones(10))
 ZEROS = pd.DataFrame(np.zeros(50))
